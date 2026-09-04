c439219a306b04a249ada33d3a6e1e5123686a5c
DrRandom <37899881+zekageri@users.noreply.github.com>
2026-09-04 10:21:20 +0200

    Route Tempo date strings through Strata

--- src/internal/tempo_date/date_allocator.h
+++ src/internal/tempo_date/date_allocator.h
@@ -1,78 +1,40 @@
 #pragma once
 
-#if __has_include(<ESPBufferManager.h>)
-#include <ESPBufferManager.h>
-#define ESP_DATE_HAS_BUFFER_MANAGER 1
-#elif __has_include(<esp_buffer_manager/buffer_manager.h>)
-#include <esp_buffer_manager/buffer_manager.h>
-#define ESP_DATE_HAS_BUFFER_MANAGER 1
-#else
-#define ESP_DATE_HAS_BUFFER_MANAGER 0
-#endif
+#include <Strata.h>
 
 #include <cstddef>
-#include <cstdlib>
 #include <limits>
-#include <new>
 #include <string>
-
-namespace date_allocator_detail {
-inline void *allocate(std::size_t bytes, bool usePSRAMBuffers) noexcept {
-#if ESP_DATE_HAS_BUFFER_MANAGER
-	return ESPBufferManager::allocate(bytes, usePSRAMBuffers);
-#else
-	(void)usePSRAMBuffers;
-	return std::malloc(bytes);
-#endif
-}
-
-inline void deallocate(void *ptr) noexcept {
-#if ESP_DATE_HAS_BUFFER_MANAGER
-	ESPBufferManager::deallocate(ptr);
-#else
-	std::free(ptr);
-#endif
-}
-} // namespace date_allocator_detail
 
 template <typename T> class DateAllocator {
   public:
 	using value_type = T;
 
 	DateAllocator() noexcept = default;
-	explicit DateAllocator(bool usePSRAMBuffers) noexcept : usePSRAMBuffers_(usePSRAMBuffers) {
+	explicit DateAllocator(Strata::Placement placement) noexcept : placement_(placement) {
 	}
 
 	template <typename U>
-	DateAllocator(const DateAllocator<U> &other) noexcept
-	    : usePSRAMBuffers_(other.usePSRAMBuffers()) {
+	DateAllocator(const DateAllocator<U> &other) noexcept : placement_(other.placement()) {
 	}
 
 	T *allocate(std::size_t n) {
-		if (n == 0) {
+		if (n == 0 || n > (std::numeric_limits<std::size_t>::max() / sizeof(T))) {
 			return nullptr;
 		}
-		if (n > (std::numeric_limits<std::size_t>::max() / sizeof(T))) {
-			return nullptr;
-		}
-
-		void *memory = date_allocator_detail::allocate(n * sizeof(T), usePSRAMBuffers_);
-		if (memory == nullptr) {
-			return nullptr;
-		}
-		return static_cast<T *>(memory);
+		return Strata::allocateArray<T>(n, placement_);
 	}
 
 	void deallocate(T *ptr, std::size_t) noexcept {
-		date_allocator_detail::deallocate(ptr);
+		Strata::free(ptr);
 	}
 
-	bool usePSRAMBuffers() const noexcept {
-		return usePSRAMBuffers_;
+	Strata::Placement placement() const noexcept {
+		return placement_;
 	}
 
 	template <typename U> bool operator==(const DateAllocator<U> &other) const noexcept {
-		return usePSRAMBuffers_ == other.usePSRAMBuffers();
+		return placement_ == other.placement();
 	}
 
 	template <typename U> bool operator!=(const DateAllocator<U> &other) const noexcept {
@@ -82,7 +44,7 @@ template <typename T> class DateAllocator {
   private:
 	template <typename> friend class DateAllocator;
 
-	bool usePSRAMBuffers_ = false;
+	Strata::Placement placement_ = Strata::Placement::PreferExternal;
 };
 
 using DateString = std::basic_string<char, std::char_traits<char>, DateAllocator<char>>;
